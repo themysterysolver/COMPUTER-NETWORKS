7fae980864f8ad5eb060e344fbe7decff5386620
themysterysolver <arjunprabhakar1910@gmail.com>
2024-08-13 05:29:33 +0530

    FEAT:server for echo program

--- WEEK-1/TCP/server.c
+++ WEEK-1/TCP/server.c
@@ -5,9 +5,28 @@
 #include <stdlib.h>     //used for exit( )
 #include <string.h>     //used for strlen()
 #define PORT 8080
+#define MAX 100
+void func(int clifd){
+    char buff[MAX];
+    int n;
+    while(1){
+        bzero(buff,MAX);
+        int bytes=recv(clifd,buff,sizeof(buff),0);
+        if(bytes<=0){
+            printf("error occured while recieveing!!");
+            break;
+        }
+        printf("From client:%s",buff);
+        send(clifd,buff,sizeof(buff),0);
+        if(!strncmp("exit",buff,4)){
+            printf("Server exit");
+            break;
+        }
+    }
+}
 int main(){
-    int sockfd,connfd,len;
-    struct sockaddr_in serveraddr,cli;
+    int sockfd,clifd,len;
+    struct sockaddr_in serveraddr,cliaddr;
 
     //socket creation!
     sockfd=socket(AF_INET,SOCK_STREAM,0);
@@ -21,6 +40,33 @@ int main(){
     bzero(&serveraddr,sizeof(serveraddr));
     
     serveraddr.sin_family=AF_INET;
-    serveraddr.sin_
+    serveraddr.sin_addr.s_addr=htonl(INADDR_ANY);
+    serveraddr.sin_port=htonl(PORT);
+
+    if((bind(sockfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr)))==0){
+        printf("Socket is binded sucessfullt to the address!!");
+    }
+    else{
+        printf("Binding of socket failed!!");
+        exit(0);
+    }
+
+    if(listen(sockfd,3)!=-1){
+        printf("Server is now listening!!");
+    }
+    else{
+        printf("Listening failed!!");
+        exit(0);
+    }
+    len=sizeof(cliaddr);
+    clifd=accept(sockfd,(struct sockaddr*)&cliaddr,&len);
+    if(clifd!=-1){
+        printf("aceptig the clinet is sucessful!!");
+    }
+    else{
+        printf("aceept() failed!!");
+    }
+    func(clifd);
+    close(sockfd);
 }
 
